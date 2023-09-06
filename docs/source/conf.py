ec1f88438373edc56290f4a281345700914ee764
Richard Leggett <richard.leggett@earlham.ac.uk>
2023-09-06 19:48:43 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -92,6 +92,23 @@ todo_include_todos = True
 # documentation.
 #
 # html_theme_options = {}
+html_theme_options = {
+    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
