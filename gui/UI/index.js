ac2e216bc181d594975e47cb4a8c3769f66fb1fa
Ned Peel <13928281+nedpeel@users.noreply.github.com>
2023-09-11 16:28:52 +0100

    Update gui - v0.19.2

--- gui/UI/index.js
+++ gui/UI/index.js
@@ -42,58 +42,6 @@ serverOptions["TaxonomyDirectory"] = "";
 serverOptions["MaxSimultaneousAnalyses"] = 10;
 var numAnalyses = 0;
 
-// var serverOptionsPath = "";
-// if(fsExtra.existsSync("./marti_server_options.txt")) {
-//   serverOptionsPath = "./marti_server_options.txt";
-// } else if(fsExtra.existsSync(homedir + "/marti_server_options.txt")) {
-//   serverOptionsPath = homedir + "/marti_server_options.txt";
-// } else if (fsExtra.existsSync(homedir + "/.marti_server_options.txt")) {
-//   serverOptionsPath = homedir + "/.marti_server_options.txt";
-// } else {
-//   console.log("Warning: Could not find marti_server_options.txt.");
-//   console.log("You must have the file marti_server_options.txt in your home directory to start new analyses.");
-// }
-
-// try {
-//   const MARTiServerOptions = fsExtra.readFileSync(serverOptionsPath, 'UTF-8');
-//   const lines = MARTiServerOptions.split(/\r?\n/);
-//   lines.forEach((line) => {
-//       if(line.charAt(0) != '#') {
-//         const fields = line.split("\t");
-//         if(fields[0] == "MinKNOWRunDirectory") {
-//           serverOptions["MinKNOWRunDirectory"] = fields[1];
-//         } else if(fields[0] == "MARTiSampleDirectory") {
-//           const dirs = fields[1].split(":");
-//           for (const dir of dirs) {
-//             var finalDir;
-//             if (dir.endsWith('/')){
-//               finalDir = dir.slice(0, -1);
-//             } else {
-//               finalDir = dir;
-//             };
-//             serverOptions["MARTiSampleDirectory"].push(finalDir);
-//           }
-//
-//         } else if (fields[0] == "BlastDatabaseDirectory") {
-//           serverOptions["BlastDatabaseDirectory"] = fields[1];
-//         } else if (fields[0] == "TaxonomyDirectory") {
-//           serverOptions["TaxonomyDirectory"] = fields[1];
-//         } else if (fields[0] == "MaxSimultaneousAnalyses") {
-//           serverOptions["MaxSimultaneousAnalyses"] = parseInt(fields[1]);
-//         }
-//       }
-//   });
-//   if( serverOptions["MinKNOWRunDirectory"] == "" ||
-//       serverOptions["MARTiSampleDirectory"].length < 1 ||
-//       serverOptions["BlastDatabaseDirectory"] == "" ||
-//       serverOptions["TaxonomyDirectory"] == "") {
-//     console.log("Warning: Could not find all fields in " + serverOptionsPath + ".");
-//     console.log("Please check this file and restart to start new analyses.");
-//   }
-// } catch (err) {
-//
-// }
-
 
 var engineOptionsPath = "";
 if (argv.options) {
@@ -108,7 +56,6 @@ if (argv.options) {
     engineOptionsPath = homedir + "/.marti_engine_options.txt";
   } else {
     console.log("Warning: Could not find marti_engine_options.txt");
-    // console.log("You must have the file marti_engine_options.txt in your home directory to start new analyses.");
   }
 
 
@@ -271,40 +218,6 @@ function makeConfigFileString(form_object) {
               configFileString += "\n";
         }
   }
-
-  // if(form_object.hasOwnProperty("processName")) {
-  //   if(Array.isArray(form_object["processName"])) {
-  //   for(var i = 0; i < form_object["processName"].length; i++) {
-  //       configFileString += "BlastProcess\n";
-  //       configFileString += "\tName:" + form_object["processName"][i] + "\n";
-  //       configFileString += "\tProgram:" + form_object["blastProgram"][i] + "\n";
-  //       configFileString += "\tDatabase:" + form_object["databaseDir"][i] + "/" + form_object["blastDatabase"][i] + "\n";
-  //       if(form_object["taxaFilter"][i].length > 0) {
-  //         configFileString += "\tTaxaFilter:" + form_object["taxaFilter"][i] + "\n";
-  //       }
-  //       configFileString += "\tMaxE:" + form_object["maxE"][i] + "\n";
-  //       configFileString += "\tMaxTargetSeqs:" + form_object["maxTargetSeqs"][i] + "\n";
-  //       configFileString += "\tBlastThreads:" + form_object["blastThreads"][i] + "\n";
-  //       if(form_object.hasOwnProperty("useToClassify") && form_object["useToClassify"][i] == "on") {
-  //         configFileString += "\tUseToClassify\n";
-  //       }
-  //     }
-  //   } else {
-  //     configFileString += "BlastProcess\n";
-  //     configFileString += "\tName:" + form_object["processName"] + "\n";
-  //     configFileString += "\tProgram:" + form_object["blastProgram"] + "\n";
-  //     configFileString += "\tDatabase:" + form_object["databaseDir"] + "/" + form_object["blastDatabase"] + "\n";
-  //     if(form_object["taxaFilter"].length > 0 ) {
-  //       configFileString += "\tTaxaFilter:" + form_object["taxaFilter"] + "\n";
-  //     }
-  //     configFileString += "\tMaxE:" + form_object["maxE"] + "\n";
-  //     configFileString += "\tMaxTargetSeqs:" + form_object["maxTargetSeqs"] + "\n";
-  //     configFileString += "\tBlastThreads:" + form_object["blastThreads"] + "\n";
-  //     if(form_object.hasOwnProperty("useToClassify") && form_object["useToClassify"] == "on") {
-  //       configFileString += "\tUseToClassify\n";
-  //     }
-  //   }
-  // }
   return configFileString;
 }
 
@@ -763,15 +676,22 @@ io.on('connect', function(socket){
     var originalId = request.originalId;
     var sampleId = request.pathName;
     var runId = request.pathRun;
+
     var dir;
     var idFileContent={};
       if (sampleMetaDict[runId]) {
-        dir = sampleMetaDict[runId][sampleId]["sample"]["dir"]
+        dir = sampleMetaDict[runId][sampleId]["sample"]["dir"];
         var idFilePath = dir + "/" + runId + "/ids.json";
 
         if (fsExtra.existsSync(idFilePath)) {
-          idFileContent = fsExtra.readFileSync(idFilePath);
-          idFileContent = JSON.parse(idFileContent);
+          idFileContentTemp = fsExtra.readFileSync(idFilePath);
+          try {
+            idFileContentTemp = JSON.parse(idFileContentTemp);
+            idFileContent = idFileContentTemp;
+
+          } catch (error) {
+            console.error(error);
+          }
         }
 
         idFileContent[originalId] = newId;
@@ -802,7 +722,6 @@ io.on('connect', function(socket){
           runId: runId
         };
         io.to(id).emit('current-dashboard-sample-response', clientData[id].selectedDashboardSample);
-        // console.log(`[${new Date().toLocaleString()}][${id}] Dashboard sample selected: ${runId} - ${sampleId}`);
   });
 
   socket.on('selected-compare-samples', samples => {
@@ -814,7 +733,6 @@ io.on('connect', function(socket){
       for (const sample of samples) {
         sampleNames.push(sample.name);
       }
-      // console.log(`[${new Date().toLocaleString()}][${id}] Compare samples selected: ${sampleNames}`);
   });
 
   socket.on('current-dashboard-sample-request', request => {
