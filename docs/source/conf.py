6f71ef451643f888ddd58c304e632ba1a5e0558f
Richard Leggett <richard.leggett@earlham.ac.uk>
2023-10-10 10:23:55 +0100

    Updated

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,8 @@
 extensions = ['sphinx.ext.todo',
     'sphinx.ext.imgmath',
     'sphinx.ext.ifconfig',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.githubpages',
+    'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
