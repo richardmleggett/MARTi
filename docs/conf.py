268f22089531e45889a32a9df4679e13c181fd2f
Richard Leggett <richard.leggett@earlham.ac.uk>
2023-10-10 10:36:44 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,8 @@
 extensions = ['sphinx.ext.todo',
     'sphinx.ext.imgmath',
     'sphinx.ext.ifconfig',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.githubpages',
+    'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -89,6 +90,7 @@ todo_include_todos = True
 #html_theme = 'sphinx_rtd_theme'
 #html_theme = 'classic'
 #html_theme = 'master'
+html_theme = "sphinx_rtd_theme"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
