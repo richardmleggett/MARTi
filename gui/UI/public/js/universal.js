c44a65783e984d7944c7a40adbda5362bab72a7f
Ned Peel <13928281+nedpeel@users.noreply.github.com>
2025-05-28 12:08:28 +0100

    Update gui

--- gui/UI/public/js/universal.js
+++ gui/UI/public/js/universal.js
@@ -341,6 +341,15 @@ socket.on('register-response', response => {
   restrictedMode = response.mode;
   console.log("id: " + response.id);
   console.log("mode: " + restrictedMode);
+
+  socket.emit('client-version-request',{
+    clientId: uuid
+  });
+
+  if (currentPage == "Samples"){
+    initialiseSamplePage();
+  };
+
 });
 
 
